61fcbfcb1a5238cb02e3a45a633f1d86c8f8cefb
Guan <crownpku@gmail.com>
2016-08-30 23:00:49 +0800

    continue working

--- crawl_hk_ipo.py
+++ crawl_hk_ipo.py
@@ -1,11 +1,45 @@
+#!/user/bin/env python
+# coding:utf-8
+
 import selenium
+from selenium import webdriver
 
 start_link = 'http://www.aastocks.com/tc/ipo/ListedIPO.aspx'
+phantomjs_path = '/home/guan/Software/phantomjs-2.1.1-linux-x86_64/bin/phantomjs'
+
+def get_sotcks_info(ds, outfile):
+    stocks = ds.find_elements_by_xpath('//tr[@class="DR" or @class="ADR"]')
+    for stock in stocks:
+        strtmp = ''
+        for element in stock:
+            strtmp += '\t' + element.text.strip('"').strip()
+        print strtmp
+        print >> outfile, strtmp
+    return 0
 
 def crawl_hk_ipo():
-    ds = selenium.webdriver(phantomjs)
-    ds.get(start_link)
-    ds.find_element_by_xpath('//body/div[@class = "xxx"]/dic[@class = 'yyy']')
+    outfile = open('hongkongipo', 'w')
+    ds = webdriver.PhantomJS(executable_path=phantomjs_path)
+    ds.implicitly_wait(10)
+    ds.set_page_load_timeout(10)
+    ds.maximize_window()
+    crawl_finish = 0
 
+    while crawl_finish == 0:
+        ds.get(start_link)
+        get_sotcks_info(ds, outfile)
+        cur_page_num = str(ds.find_element_by_xpath('//div[@class="paging_cur"]').text)
+        cur_page_num = int(cur_page_num)
 
-    return 0
+        try:
+            print 'Crawling page' + str(cur_page_num+1)
+            next_page_button = ds.find_element_by_xpath('//a[@href=""javascript:searchPage('+ str(cur_page_num+1) + ')]')
+            next_page_button.click()
+        except:
+            print 'Crawling Finished!'
+            crawl_finish = 1
+
+    return  0
+
+if __name__ == '__main__':
+    crawl_hk_ipo()
