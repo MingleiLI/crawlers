a89358dd47669aac11bf9e34a8d1f5b20d773c4d
crownpku <crownpku@gmail.com>
2017-07-05 12:51:54 +0800

    ccgb add multithreading

--- ccgp_crawler/ccgp_crawler.py
+++ ccgp_crawler/ccgp_crawler.py
@@ -1,9 +1,11 @@
 #encoding = utf-8
 from selenium import webdriver
+import sys
 phantomjs_path = '/home/guan/Software/phantomjs-2.1.1-linux-x86_64/bin/phantomjs'
 target_link_1 = 'http://search.ccgp.gov.cn/bxsearch?searchtype=1&page_index='
 target_link_2 ='&bidSort=0&buyerName=&projectId=&pinMu=0&bidType=7&dbselect=bidx&kw=&start_time=2013%3A01%3A01&end_time=2017%3A07%3A05&timeType=6&displayZone=&zoneId=&pppStatus=0&agentName='
-max_index = 80504
+index_range = 8000
+thread_num = 10
 
 
 class crawl_ccgp(object):
@@ -24,11 +26,13 @@ class crawl_ccgp(object):
 
 
 if __name__ == "__main__":
-    outfile = open('ccgp_list_20170705.txt', 'a')
-    i = 0
+    cur_thr_index = int(sys.argv[1])
+    start_index = (cur_thr_index-1) * index_range + 1
+    end_index = cur_thr_index * index_range
+    outfile = open('ccgp_list_' + str(cur_thr_index) + '.txt', 'a')
+    i = start_index
     phantom_driver = webdriver.PhantomJS(executable_path=phantomjs_path)
-    while i <= max_index:
-        i += 1
+    while i <= end_index:
         print 'Crawling Page ' + str(i) 
         target_link = target_link_1 + str(i) + target_link_2
         fail_count = 0
@@ -39,4 +43,5 @@ if __name__ == "__main__":
             except:   
                 fail_count += 1
                 print 'Fail ' + str(fail_count) + ' times at page ' + str(i)
+        i += 1
                 
