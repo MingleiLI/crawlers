586a06b4864ae5a00d915c83413e1a0a47595bf3
crownpku <crownpku@gmail.com>
2017-07-22 15:24:43 +0800

    add mingluji content crawler

--- mingluji_crawler/mingluji_content_crawler.py
+++ mingluji_crawler/mingluji_content_crawler.py
@@ -1,62 +1,51 @@
 # -*- coding: utf8 -*-
 from selenium import webdriver
+import sys
 phantomjs_path = '/home/gwang3/software/phantomjs-2.1.1-linux-x86_64/bin/phantomjs'
 
-class crawl_ccgp(object):
+class crawl_mingluji(object):
     def __init__(self, phantom_driver):
         self.ds = phantom_driver
         self.ds.set_page_load_timeout(8)
+        self.item_header = ['机构名称', '机构类型', '经营范围', '经济类型', '注册日期', '办证日期', '办证机构', '注册资金', '职工人数', '经济行业', '门类', '大类', '中类', '小类', '法人代表', '长途区号', '电话号码', '中国行政区号', '省份', '地市', '区县', '机构地址', '邮政编码']
     
-    def crawl_list(self, title, target_link):
-        file_name = ''
-        '''
-        gg_types = ['需求', '招标', '更正', '中标', '废标']
-        gg_cur_type = '其它'
-        for item in gg_types:
-            if item in title:
-                gg_cur_type = item
-                break
-        '''
-        
-        
+    def crawl_list(self, title, target_link, outfile):
         self.ds.get(target_link)
-        gg_cur_type = self.ds.find_element_by_xpath('//a[@class="current"]').text.strip().encode('utf-8')
-        text1 = self.ds.find_element_by_xpath('//div[@class="content-right-details-top"]/p').text
-        date = text1.strip().split()[0].strip()
-        if u'：' in date:
-            date = date.split(u'：')[1].strip()
-        elif ':' in date:
-            date = date.split(':')[1].strip()
-        file_name = gg_cur_type
-        file_name = file_name + '_' + date.encode('utf-8')
-        file_name = file_name + '_' + title
-        print file_name
-        text2 = self.ds.find_element_by_xpath('//div[@class="content-right-details-content"]').get_attribute('innerHTML')
-        text2text = self.ds.find_element_by_xpath('//div[@class="content-right-details-content"]').text
-        outfile = open('./bgpc_raw_html/' + file_name, 'w')
-        print >> outfile, str(text2.encode('utf-8'))
-        outfile.close()
-        outfile = open('./bgpc_raw_txt/' + file_name + '.txt', 'w')
-        print >> outfile, str(text2text.encode('utf-8'))
-        outfile.close()
-        self.ds.get('www.google.com')
-                                            
-        
-        
-
+        try:
+            description = self.ds.find_element_by_xpath('//span[@itemprop="description"]').text.encode('utf-8')
+        except:
+            description = 'NA'
+        items = self.ds.find_elements_by_xpath('//ul/li')
+        item_dic = {}
+        for item in items:
+            strtmp = item.text.encode('utf-8')
+            if '：' in strtmp:
+                strtmp = strtmp.split('：')
+            else:
+                continue
+            if strtmp[1].strip() != "":
+                item_dic[strtmp[0]] = strtmp[1].strip()
+        strtmp = ""
+        for item in self.item_header:
+            if item in item_dic:
+                strtmp = strtmp + '\t' + item_dic[item]
+            else:
+                strtmp = strtmp + '\t' + 'NA'
+        strtmp = strtmp + '\t' + description
+        print>> outfile, strtmp.strip()
+            
 
 if __name__ == "__main__":
     
-    #outfile = open('ccgp_list_' + str(cur_thr_index) + '.txt', 'a')
     
     count = 0
     phantom_driver = webdriver.PhantomJS(executable_path=phantomjs_path)
-    infile = open('bgpc_list_it_new')
+    file_name = sys.argv[1]
+    infile = open('./data/' + file_name )
+    outfile = open('./data/' + file_name + '_content', 'w')
     for n, line in enumerate(infile):
         #if n < 837:
         #    continue
-        
-        #outfile = open('./text_data_bgpc_20170713/' + str(n), 'w')
         fail_count = 0
         while(fail_count < 5):
             try:
@@ -64,10 +53,13 @@ if __name__ == "__main__":
                 #print >> outfile, title
                 #print >> outfile, '=========='
                 print 'Crawling Page ' + str(n) 
-                crawl_ccgp(phantom_driver).crawl_list(title, target_link)
+                crawl_mingluji(phantom_driver).crawl_list(title, target_link, outfile)
                 break
             
             except Exception as e:
                 print e
                 fail_count += 1
                 print 'Fail ' + str(fail_count) + ' times at page ' + str(n)
+                
+                
+                
