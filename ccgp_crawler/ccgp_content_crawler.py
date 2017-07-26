d8370fb6127b47390d39e0e778a69dc2daf43d46
crownpku <crownpku@gmail.com>
2017-07-26 12:12:36 +0800

    update ccgp

--- ccgp_crawler/ccgp_content_crawler.py
+++ ccgp_crawler/ccgp_content_crawler.py
@@ -1,16 +1,31 @@
-#encoding = utf-8
+#coding=utf-8
 from selenium import webdriver
+import time
 phantomjs_path = '/home/gwang3/software/phantomjs-2.1.1-linux-x86_64/bin/phantomjs'
 
 class crawl_ccgp(object):
     def __init__(self, phantom_driver):
         self.ds = phantom_driver
-        self.ds.set_page_load_timeout(8)
+        
     
-    def crawl_list(self, target_link, outfile):
+    def crawl_list(self, target_link, outfile_table, outfile_detail):
         self.ds.get(target_link)
+        time.sleep(3)
+        try:
+            titles = self.ds.find_elements_by_xpath('//td[@class="title"]')
+            contents = self.ds.find_elements_by_xpath('//td[@class="title"]/following-sibling::td')
+            for n, title in enumerate(titles):
+                title = title.text.encode('utf-8')
+                content = contents[n].text.encode('utf-8')
+                strtmp = title + '\t' + content
+                print >> outfile_table, strtmp
+            button = self.ds.find_element_by_xpath('//span[@id="displayGG"]')
+            button.click()
+        except:
+            print 'No Table!'
+            
         text = self.ds.find_element_by_xpath('//div[@class="vT_detail_content w760c"]').text
-        print >> outfile, str(text.encode('utf-8'))
+        print >> outfile_detail, str(text.encode('utf-8'))
                                             
         
         
@@ -21,24 +36,48 @@ if __name__ == "__main__":
     #outfile = open('ccgp_list_' + str(cur_thr_index) + '.txt', 'a')
     
     count = 0
-    phantom_driver = webdriver.PhantomJS(executable_path=phantomjs_path)
-    infile = open('ccgp_list_it')
+    #Set Headers
+    headers = {'Accept': '*/*',
+               'Accept-Encoding': 'gzip, deflate, sdch',
+               'Accept-Language': 'en-US,en;q=0.8',
+               'Cache-Control': 'max-age=0',
+               'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+               }
+
+    for key, value in enumerate(headers):
+        webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.{}'.format(key)] = value
+
+    service_args = []
+    phantom_driver = webdriver.PhantomJS(executable_path=phantomjs_path, service_args=service_args)
+    phantom_driver.implicitly_wait(10)
+    phantom_driver.set_page_load_timeout(8)
+    phantom_driver.maximize_window()
+    
+    infile = open('ccgp_list_fuwuqi_zbgg_1')
     for n, line in enumerate(infile):
-        if n < 116:
-            continue
-        outfile = open('./text_data/' + str(n), 'w')
-        title, target_link = line.strip().split('|')
-        print >> outfile, title 
-        print >> outfile, '=========='
+        #if n < 116:
+        #    continue
+        outfile_table = open('./text_data_ccgp_fuwuqi_zbgg/' + str(n) + '_table', 'w')
+        outfile_detail = open('./text_data_ccgp_fuwuqi_zbgg/' + str(n) + '_detail', 'w')
+        title, target_link, loc = line.strip().split('|')
+        
+        print >> outfile_table, title
+        print >> outfile_table, loc
+        print >> outfile_table, '=========='
+        print >> outfile_detail, title
+        print >> outfile_detail, loc
+        print >> outfile_detail, '=========='
         print 'Crawling Page ' + str(n) 
+        print target_link
         fail_count = 0
         while(fail_count < 5):
             try:
-                crawl_ccgp(phantom_driver).crawl_list(target_link, outfile)
+                crawl_ccgp(phantom_driver).crawl_list(target_link, outfile_table, outfile_detail)
                 break
             except Exception as e:
                 print e
                 fail_count += 1
                 print 'Fail ' + str(fail_count) + ' times at page ' + str(n)
-        outfile.close()
+        outfile_table.close()
+        outfile_detail.close()
                 
--- ccgp_crawler/ccgp_crawler.py
+++ ccgp_crawler/ccgp_crawler.py
@@ -1,33 +1,37 @@
-#encoding = utf-8
+#coding=utf-8
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import sys
 import time
 phantomjs_path = '/home/gwang3/software/phantomjs-2.1.1-linux-x86_64/bin/phantomjs'
 target_link_1 = 'http://search.ccgp.gov.cn/bxsearch?searchtype=1&page_index='
-target_link_2 ='&bidSort=0&buyerName=&projectId=&pinMu=0&bidType=7&dbselect=bidx&kw=&start_time=2013%3A01%3A01&end_time=2017%3A07%3A05&timeType=6&displayZone=&zoneId=&pppStatus=0&agentName='
-index_range = 8000
-thread_num = 10
+#bidType=1 招标公告
+#bigType=7 中标公告
+target_link_2 ='&bidSort=0&buyerName=&projectId=&pinMu=0&bidType=1&dbselect=bidx&kw=%E6%9C%8D%E5%8A%A1%E5%99%A8&start_time=2013%3A01%3A01&end_time=2017%3A07%3A05&timeType=6'
+index_range = 204
+thread_num = 1
 
 
 class crawl_ccgp(object):
     def __init__(self, phantom_driver):
         self.ds = phantom_driver
-        self.ds.set_page_load_timeout(8)
-    
+        
     def crawl_list(self, target_link, outfile):
         print target_link
         self.ds.get(target_link)
-        time.sleep(5)
-        print self.ds.page_source
+        #time.sleep(3)
+        #outfileee = open('test.html', 'w')
+        #print >> outfileee, self.ds.page_source.encode('utf-8')
         items = self.ds.find_elements_by_xpath('//ul[@class="vT-srch-result-list-bid"]/li/a')
-        print items
-        for item in items:
-            print item
-            link = item.find_element_by_xpath('//a').get_attribute('href')
-            text = item.find_element_by_xpath('//a').text.strip()
-            loc = item.find_element_by_xpath('//span/a').text.strip()
-            print loc
-            print >> outfile, str(text.encode('utf-8')) + '|' + str(link) + '|' + str(loc)
+        locs = self.ds.find_elements_by_xpath('//ul[@class="vT-srch-result-list-bid"]/li/span/a')
+        #print items
+        for n, item in enumerate(items):
+            #print item
+            link = item.get_attribute('href')
+            text = item.text.strip()
+            loc = locs[n].text.strip()
+            #print loc
+            print >> outfile, str(text.encode('utf-8')) + '|' + str(link.encode('utf-8')) + '|' + str(loc.encode('utf-8'))
                                             
         
         
@@ -37,9 +41,26 @@ if __name__ == "__main__":
     cur_thr_index = int(sys.argv[1])
     start_index = (cur_thr_index-1) * index_range + 1
     end_index = cur_thr_index * index_range
-    outfile = open('ccgp_list_loc_' + str(cur_thr_index) + '.txt', 'a')
+    outfile = open('ccgp_list_fuwuqi_gkzb_' + str(cur_thr_index) , 'a')
     i = start_index
-    phantom_driver = webdriver.PhantomJS(executable_path=phantomjs_path)
+    
+    #Set Headers
+    headers = {'Accept': '*/*',
+               'Accept-Encoding': 'gzip, deflate, sdch',
+               'Accept-Language': 'en-US,en;q=0.8',
+               'Cache-Control': 'max-age=0',
+               'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+               }
+
+    for key, value in enumerate(headers):
+        webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.{}'.format(key)] = value
+
+    service_args = []
+    phantom_driver = webdriver.PhantomJS(executable_path=phantomjs_path, service_args=service_args)
+    phantom_driver.implicitly_wait(10)
+    phantom_driver.set_page_load_timeout(8)
+    phantom_driver.maximize_window()
+    
     while i <= end_index:
         print 'Crawling Page ' + str(i) 
         target_link = target_link_1 + str(i) + target_link_2
